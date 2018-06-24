2b8b885f0d5e49c754eeca273f62b9609ee30a0e
Maximilian Wilhelm <max@sdn.clinic>
2018-06-24 10:42:11 +0200

    Add configuration parameters for route reflection (RFC4456)
    Signed-off-by: Maximilian Wilhelm <max@sdn.clinic>

--- config/peer.go
+++ config/peer.go
@@ -8,20 +8,23 @@ import (
 	"github.com/bio-routing/bio-rd/routingtable/filter"
 )
 
+// Peer type defines configuration options for any BGP peer
 type Peer struct {
-	AdminEnabled      bool
-	ReconnectInterval time.Duration
-	KeepAlive         time.Duration
-	HoldTime          time.Duration
-	LocalAddress      net.IP
-	PeerAddress       net.IP
-	LocalAS           uint32
-	PeerAS            uint32
-	Passive           bool
-	RouterID          uint32
-	AddPathSend       routingtable.ClientOptions
-	AddPathRecv       bool
-	ImportFilter      *filter.Filter
-	ExportFilter      *filter.Filter
-	RouteServerClient bool
+	AdminEnabled            bool
+	ReconnectInterval       time.Duration
+	KeepAlive               time.Duration
+	HoldTime                time.Duration
+	LocalAddress            net.IP
+	PeerAddress             net.IP
+	LocalAS                 uint32
+	PeerAS                  uint32
+	Passive                 bool
+	RouterID                uint32
+	AddPathSend             routingtable.ClientOptions
+	AddPathRecv             bool
+	ImportFilter            *filter.Filter
+	ExportFilter            *filter.Filter
+	RouteServerClient       bool
+	RouteReflectorClient    bool
+	RouteReflectorClusterID uint32
 }
